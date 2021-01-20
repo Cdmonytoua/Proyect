7b71e4dfc98014e69ba2dc78909f72cd0b45ed84
isaigm <isaigm23@gmail.com>
2021-01-20 08:24:28 -0600

    docker

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'facebookAuth': {
-    'clientID': '1776497042507332', // your App ID
-    'clientSecret': 'a741f400955b7d87658229896708ff55', // your App Secret
-    'callbackURL': 'http://localhost:4000/login/facebook/callback'
-  }
-}
-
